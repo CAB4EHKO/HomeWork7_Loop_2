370f931176cc562baa0de1b54cad05be571b8477
Savchenko <savchenko.ra@yandex.ru>
2023-02-22 17:23:26 +0300

    Task №1.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+    }
+    public static void task1() {
+        System.out.println("Task 1.");
+        int deposit = 15000;
+        int total = 0;
+        int month = 0;
+        while (total <= 2459000 ) {
+            total = total + deposit;
+            month++;
+            System.out.println("Месяц " + month + " сумма накоплений равна: " + total + " рублей.");
+        }
     }
 }
