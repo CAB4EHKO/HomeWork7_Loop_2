80670fefad32d48ef95b9b097576927a6f3d02c9
Savchenko <savchenko.ra@yandex.ru>
2023-02-22 22:01:22 +0300

    Task №5.

--- src/Main.java
+++ src/Main.java
@@ -4,30 +4,34 @@ public class Main {
         task2();
         task3();
         task4();
+        task5();
     }
+
     public static void task1() {
         System.out.println("Task 1.");
         int deposit = 15000;
         int total = 0;
         int month = 0;
-        while (total <= 2459000 ) {
+        while (total <= 2459000) {
             total = total + deposit;
             month++;
             System.out.println("Месяц " + month + " сумма накоплений равна: " + total + " рублей.");
         }
     }
-    public static void task2(){
+
+    public static void task2() {
         System.out.println("Task 2.");
         int i = 0;
         while (i < 10) {
             i++;
-            System.out.print (i + " ");
+            System.out.print(i + " ");
         }
         System.out.println();
         for (; i > 0; i--) {
-            System.out.print (i + " ");
+            System.out.print(i + " ");
         }
     }
+
     public static void task3() {
         System.out.println("Task 3.");
         int countryY = 12_000_000;
@@ -40,16 +44,34 @@ public class Main {
             System.out.println("Год " + years + " численость населения составляет: " + countryY + " человек.");
         }
     }
+
     public static void task4() {
         System.out.println("Task 4.");
         int sumOfMons = 15_000;
-        int totalDeposit  = 0;
+        int totalDeposit = 0;
         int month = 0;
+        int k = 7;
         while (totalDeposit < 12_000_000) {
-            sumOfMons = (sumOfMons / 100) * 7 + sumOfMons;
+            sumOfMons = (sumOfMons / 100) * k + sumOfMons;
             totalDeposit = totalDeposit + sumOfMons;
             month++;
             System.out.println("Месяц " + month + " сумма накоплений равна: " + totalDeposit + " рублей.");
+        }
+    }
+
+    public static void task5() {
+        System.out.println("Task 5.");
+        int sumOfMons = 15_000;
+        int totalDeposit = 0;
+        int month = 0;
+        int k = 7;
+        while (totalDeposit <= 12_000_000) {
+            sumOfMons = (sumOfMons / 100) * k + sumOfMons;
+            totalDeposit = totalDeposit + sumOfMons;
+            if (month % 6 == 0) {
+            System.out.println("Месяц " + month + " сумма накоплений равна: " + totalDeposit + " рублей.");
+        }
+            month++;
         }
     }
 }
